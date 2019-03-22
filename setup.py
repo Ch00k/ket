d0c4763dddc7965d2029aa0b96317e6f283058e0
Andrii Yurchuk <>
2019-03-22 22:17:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ket',
-    version='0.0.2',
+    version='0.0.3',
     description='Bitbucket workflows in your terminal',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
